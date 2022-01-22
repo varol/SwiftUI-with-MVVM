c3bfa64aba7952546a0a1b49ece2f2369c1116a3
Varol Aksoy <varolaksoy@gmail.com>
2022-01-22 20:38:52 +0300

    initial commit

---
+++ SwiftUI HackerNews With Combine/SwiftUI HackerNews With Combine/SwiftUI_HackerNews_With_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_HackerNews_With_CombineApp.swift
+//  SwiftUI HackerNews With Combine
+//
+//  Created by Varol Aksoy on 22.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_HackerNews_With_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
